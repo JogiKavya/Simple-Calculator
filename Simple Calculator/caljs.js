ccb12c08304048ead4f82ff456598e45fe2233d6
Jogi Kavya <165455035+JogiKavya@users.noreply.github.com>
2025-01-26 10:54:41 +0530

    Add files via upload

---
+++ Simple Calculator/caljs.js
@@ -0,0 +1,21 @@
+let display = document.getElementById('display');
+
+function number(number) {
+    display.value += number;
+}
+
+function operator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
